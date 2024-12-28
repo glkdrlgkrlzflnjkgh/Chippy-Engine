98177abe76d56621684af803294ec3f9c436670e
glkdrlgkrlzflnjkgh <xcallumnicx@outlook.com>
2024-12-28 08:41:39 +0000

    changed sphere2 position

--- Program.cs
+++ Program.cs
@@ -137,7 +137,7 @@ namespace MainTracer
             Vector3 origin = new Vector3(0.0f, 0.0f, 0.0f);
 
             Sphere sphere1 = new Sphere(new Vector3(0, 0, -1), 0.5f);
-            Sphere sphere2 = new Sphere(new Vector3(0f, 4f, 0f), 0.5f);
+            Sphere sphere2 = new Sphere(new Vector3(-6f, 4f, 0f), 0.9f);
 
             using (StreamWriter writer = new StreamWriter("output.ppm"))
             {
