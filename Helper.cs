85806466d504962163cdc98fbe3b4eeb49a84e99
glkdrlgkrlzflnjkgh <xcallumnicx@outlook.com>
2025-06-15 09:49:43 +0100

    Update Helper.cs

--- Helper.cs
+++ Helper.cs
@@ -1,38 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using Hardware.Info;
 using MainProg;
 
-namespace RTX 
-{
-    internal  class Helper
-    {
-        /// <summary>
-        /// returns the amount of RAM 
-        /// </summary>
-    
-        public  double GetRam() { 
-           return Engine.MemoryUsage.GetCurrentMemoryUsageInMB();
-        }
-        /// <summary>
-        /// The method that is called when there is not enough RAM to run the program.
-        /// </summary>
-        public  void NotEnoughRam() {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("You need at least 8GB of RAM to run this program.");
-            Console.ResetColor();
-            throw new OutOfMemoryException("Not enough memory to run this program.");
-        }
-        /// <summary>
-        /// This method returns the CPU core count.
-        /// </summary>
-        public  int GetCPUCoreCount() {
-            int coreCount = Environment.ProcessorCount;
-            return coreCount;
-            
-        }
-    }
+namespace RTX {
+	internal class Helper {
+		/// <summary>
+		/// Returns the amount of RAM (in GB).
+		/// </summary>
+		public double GetRam() {
+			using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem")) {
+				foreach (ManagementObject obj in searcher.Get()) {
+					return Convert.ToDouble(obj["TotalPhysicalMemory"]) / (1024 * 1024 * 1024); // Convert bytes to GB
+				}
+			}
+			return 0;
+		}
+
+		/// <summary>
+		/// The method that is called when there is not enough RAM to run the program.
+		/// </summary>
+		public void NotEnoughRam() {
+			Console.WriteLine("Error: Not enough RAM to run the program!");
+			Console.WriteLine("Consider closing unnecessary applications or upgrading your system memory.");
+			Environment.Exit(1); // Exit the program with an error code
+		}
+
+		/// <summary>
+		/// This method returns the CPU core count.
+		/// </summary>
+		public int GetCPUCoreCount() {
+			return Environment.ProcessorCount;
+		}
+	}
 }
